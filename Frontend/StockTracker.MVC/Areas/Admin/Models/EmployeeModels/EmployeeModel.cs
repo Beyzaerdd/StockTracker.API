574e055f0d139a449b4cbcceb6bed9b3b200c36f
Beyzaerdd <erdbeyza14@gmail.com>
2025-03-21 06:29:09 +0300

    ☑️Added EmployeeModels

---
+++ Frontend/StockTracker.MVC/Areas/Admin/Models/EmployeeModels/EmployeeModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StockTracker.MVC.Areas.Admin.Models.EmployeeModels
+{
+    public class EmployeeModel
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("lastname")]
+        public string LastName { get; set; }
+
+        [JsonPropertyName("phone")]
+        public string Phone { get; set; }
+
+        [JsonPropertyName("position")]
+        public string Position { get; set; }
+
+        [JsonPropertyName("salary")]
+        public decimal Salary { get; set; }
+
+        [JsonPropertyName("startdate")]
+        public DateTime StartDate { get; set; }
+
+        [JsonPropertyName("enddate")]
+        public DateTime? EndDate { get; set; }
+    }
+
+}
---
+++ Frontend/StockTracker.MVC/Areas/Admin/Models/EmployeeModels/UpdateEmployeeModel.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace StockTracker.MVC.Areas.Admin.Models.EmployeeModels
+{
+    public class UpdateEmployeeModel
+    {
+        [JsonPropertyName("id")]
+        [Required]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        [Required(ErrorMessage = "İsim alanı gereklidir.")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("lastname")]
+        [Required(ErrorMessage = "Soyisim alanı gereklidir.")]
+        public string LastName { get; set; }
+
+        [JsonPropertyName("phone")]
+        [Required(ErrorMessage = "Telefon alanı gereklidir.")]
+        [Phone(ErrorMessage = "Geçerli bir telefon numarası giriniz.")]
+        public string Phone { get; set; }
+
+        [JsonPropertyName("position")]
+        [Required(ErrorMessage = "Pozisyon alanı gereklidir.")]
+        public string Position { get; set; }
+
+        [JsonPropertyName("salary")]
+        [Required(ErrorMessage = "Maaş alanı gereklidir.")]
+        [Range(0, double.MaxValue, ErrorMessage = "Maaş negatif olamaz.")]
+        public decimal Salary { get; set; }
+
+        [JsonPropertyName("startdate")]
+        [Required(ErrorMessage = "Başlangıç tarihi gereklidir.")]
+        public DateTime StartDate { get; set; }
+
+        [JsonPropertyName("enddate")]
+        public DateTime? EndDate { get; set; }
+    }
+}
