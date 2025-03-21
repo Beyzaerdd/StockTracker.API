ca6994ee6928d0cd672e6eb10c7536ddbc2e9ad6
Beyzaerdd <erdbeyza14@gmail.com>
2025-03-21 23:34:55 +0300

    ☑️Added ProductModels

---
+++ Frontend/StockTracker.MVC/Areas/Admin/Models/ProductModels/CreateProductModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StockTracker.MVC.Areas.Admin.Models.ProductModels
+{
+    public class CreateProductModel
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("monthlyprice")]
+        public decimal MonthlyPrice { get; set; }  // Aylık kiralama ücreti
+
+        [JsonPropertyName("stockquantity")]
+        public int StockQuantity { get; set; }     // Depodaki mevcut stok
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+    }
+}
---
+++ Frontend/StockTracker.MVC/Areas/Admin/Models/ProductModels/UpdateProductModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StockTracker.MVC.Areas.Admin.Models.ProductModels
+{
+    public class UpdateProductModel
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("monthlyprice")]
+        public decimal MonthlyPrice { get; set; }  // Aylık kiralama ücreti
+
+        [JsonPropertyName("stockquantity")]
+        public int StockQuantity { get; set; }     // Depodaki mevcut stok
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+    }
+}
