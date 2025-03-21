b433d6c7587c0b2bee9c19b1dfd87234080a7773
Beyzaerdd <erdbeyza14@gmail.com>
2025-03-21 05:20:38 +0300

    ☑️Added CustomerModels

---
+++ Frontend/StockTracker.MVC/Areas/Admin/Models/CustomerModels/CreateCustomerModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace StockTracker.MVC.Areas.Admin.Models.CustomerModels
+{
+    public class CreateCustomerModel
+    {
+        [JsonPropertyName("name")]
+        [Required]
+        public string Name { get; set; }
+
+        [JsonPropertyName("lastname")]
+        [Required]
+        public string LastName { get; set; }
+
+        [JsonPropertyName("email")]
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [JsonPropertyName("phone")]
+        [Required]
+        public string Phone { get; set; }
+
+        [JsonPropertyName("address")]
+        [Required]
+        public string Address { get; set; }
+    }
+}
---
+++ Frontend/StockTracker.MVC/Areas/Admin/Models/CustomerModels/UpdateCustomerModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace StockTracker.MVC.Areas.Admin.Models.CustomerModels
+{
+    public class UpdateCustomerModel
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        [Required]
+        public string Name { get; set; }
+
+        [JsonPropertyName("lastname")]
+        [Required]
+        public string LastName { get; set; }
+
+        [JsonPropertyName("email")]
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [JsonPropertyName("phone")]
+        [Required]
+        public string Phone { get; set; }
+
+        [JsonPropertyName("address")]
+        [Required]
+        public string Address { get; set; }
+    }
+}
