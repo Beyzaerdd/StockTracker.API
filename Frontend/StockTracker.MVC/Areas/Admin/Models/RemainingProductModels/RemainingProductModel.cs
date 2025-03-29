5331ab4aecd30ed1238b74977b5c86af2313a394
Beyzaerdd <erdbeyza14@gmail.com>
2025-03-29 23:44:28 +0300

    ☑️Added new Models

---
+++ Frontend/StockTracker.MVC/Areas/Admin/Models/RemainingProductModels/RemainingProductModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StockTracker.MVC.Areas.Admin.Models.RemainingProductModels
+{
+    public class RemainingProductModel
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("productid")]
+        public int ProductId { get; set; }
+
+        [JsonPropertyName("productname")]
+        public string ProductName { get; set; }
+
+        [JsonPropertyName("daysremaining")]
+        public int DaysRemaining { get; set; }
+
+        [JsonPropertyName("dailyprice")]
+        public decimal DailyPrice { get; set; }
+
+        [JsonPropertyName("quantity")]
+        public int Quantity { get; set; }
+
+        [JsonPropertyName("totalprice")]
+        public decimal TotalPrice { get; set; }
+
+        [JsonPropertyName("quantityremaining")]
+        public int QuantityRemaining { get; set; }
+    }
+}
---
+++ Frontend/StockTracker.MVC/Areas/Admin/Models/RemainingProductModels/UpdateRemainingProductModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StockTracker.MVC.Areas.Admin.Models.RemainingProductModels
+{
+    public class UpdateRemainingProductModel
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("daysremaining")]
+        public int DaysRemaining { get; set; }
+
+        [JsonPropertyName("quantityremaining")]
+        public int QuantityRemaining { get; set; }
+    }
+}
